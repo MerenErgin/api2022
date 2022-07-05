b1868b4962ec5d0e8905a4da7cf2458bd1a1d0e6
Mehmet Eren Ergin <merenergin35@gmail.com>
2022-07-05 22:37:47 +0300

    05/07

---
+++ src/test/java/Get02.java
@@ -0,0 +1,50 @@
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.Assert.*;
+
+public class Get02 {
+    /*
+        Given
+            https://restful-booker.herokuapp.com/booking/1005
+        When
+            User send a GET Request to the url
+        Then
+            HTTP Status code should be 404
+        And
+            Status Line should be HTTP/1.1 404 Not Found
+        And
+            Response body contains "Not Found"
+        And
+            Response body does not contain "TechProEd"
+        And
+            Server is "Cowboy"
+     */
+
+    @Test
+    public void get01(){
+        //1. Step: Set the Url
+        String url= "https://restful-booker.herokuapp.com/booking/1005";
+
+        //2. Step: Set the expected data(Post- Put- Patch)
+
+        //3. Step: Type code to send request
+        Response response = given().when().get(url);
+
+        response.prettyPrint();
+
+        //4.Step: Do Assertion
+        response.then().assertThat().statusCode(404).statusLine("HTTP/1.1 404 Not Found");
+
+        //Response body'de bulunan spesifik bir veri nasil assert edilir?
+        assertTrue(response.asString().contains("Not Found"));
+
+        //Response body'de bulunmayan spesifik bir veri nasil assert edilir?
+        assertFalse(response.asString().contains("TechProEd"));
+
+        assertEquals("Cowboy",response.header("Server"));
+
+
+    }
+}
