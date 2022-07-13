a49be2f10a5b0454ae4419762baa0373cb216c85
Mehmet Eren Ergin <merenergin35@gmail.com>
2022-07-13 23:34:57 +0300

    13/07

--- src/test/java/get_requests/Get06.java
+++ src/test/java/get_requests/Get06.java
@@ -1,6 +1,16 @@
 package get_requests;
 
-public class Get06 {
+import base_urls.HerokuappBaseUrl;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+import static junit.framework.TestCase.assertEquals;
+import static org.hamcrest.CoreMatchers.equalTo;
+
+public class Get06 extends HerokuappBaseUrl {
     /*
     Given
             https://restful-booker.herokuapp.com/booking/555
@@ -13,15 +23,53 @@ public class Get06 {
         And
             Response body should be like;
           {
-            "firstname": "Sally",
-            "lastname": "Brown",
-            "totalprice": 111,
+            "firstname": "Itzel",
+            "lastname": "Villanueva",
+            "totalprice": 593,
             "depositpaid": true,
             "bookingdates": {
-                "checkin": "2013-02-23",
-                "checkout": "2014-10-23"
+                "checkin": "2022-07-13",
+                "checkout": "2022-07-26"
             },
-            "additionalneeds": "Breakfast"
+            "additionalneeds": "Lunch"
         }
      */
+
+    @Test
+    public void get01(){
+        //1. Step: Set the Url
+        spec.pathParams("first","booking","second",555);
+
+        //2,Step: Set the expected data
+
+        //3. Step: Send the request and get the response
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+        //response.prettyPrint();
+
+        //4. Step: Do Assertion
+        //1.yol:
+        response.then().
+                assertThat().
+                statusCode(200).
+                contentType(ContentType.JSON).
+                body("firstname",equalTo("Itzel")
+                        ,"lastname",equalTo("Villanueva")
+                        ,"totalprice",equalTo(593)
+                        ,"depositpaid",equalTo(true)
+                        ,"bookingdates.checkin",equalTo("2022-07-13")
+                        ,"bookingdates.checkout",equalTo("2022-07-26")
+                );
+
+        //2.yol: JsonPath Class kullanilir
+        JsonPath json = response.jsonPath();
+
+        assertEquals("Itzel",json.getString("firstname"));
+        assertEquals("Villanueva",json.getString("lastname"));
+        assertEquals(593,json.getInt("totalprice"));
+        assertEquals(true,json.getBoolean("depositpaid"));
+        assertEquals("2022-07-13",json.getString("bookingdates.checkin"));
+        assertEquals("2022-07-26",json.getString("bookingdates.checkout"));
+
+    }
+
 }
