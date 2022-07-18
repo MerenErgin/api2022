3034557db004d806a98ae204c3a14deb2984acb0
Mehmet Eren Ergin <merenergin35@gmail.com>
2022-07-18 13:55:50 +0300

    18/07

--- src/test/java/get_requests/Get08.java
+++ src/test/java/get_requests/Get08.java
@@ -3,6 +3,7 @@ package get_requests;
 import base_urls.JsonPlaceHolderBaseUrl;
 import io.restassured.response.Response;
 import org.junit.Test;
+import test_data.jsonPlaceHolderTestData;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -45,6 +46,7 @@ public class Get08 extends JsonPlaceHolderBaseUrl {
         spec.pathParams("first","todos","second",2);
 
         //2. Step: Set the expected data
+           //Key  , Value   (Value'nun object olmasinin sebebi int deger de var boolean da var String de var. Object hepsini kapsior)
         Map<String, Object> expectedData= new HashMap<>();
 
         expectedData.put("userId",1);
@@ -68,6 +70,37 @@ public class Get08 extends JsonPlaceHolderBaseUrl {
         assertEquals(expectedData.get("Status code"),response.getStatusCode());
         assertEquals(expectedData.get("Via"),response.getHeader("Via"));
         assertEquals(expectedData.get("Server"),response.getHeader("Server"));
+
+    }
+
+    @Test
+    public void  get02(){
+        //1. Step: Set the Url
+        spec.pathParams("first","todos","second",2);
+
+        //2. Step: Set the expected data
+        jsonPlaceHolderTestData expectedData = new jsonPlaceHolderTestData();
+
+        Map<String, Object> expectedDataMap = expectedData.expectedDataWithAllKeys(1,"quis ut nam facilis et officia qui",false);
+
+        expectedDataMap.put("Status code",200);
+        expectedDataMap.put("Via","1.1 vegur");
+        expectedDataMap.put("Server","cloudflare");
+
+        //3. Step: Send the request and get the response
+        Response response= given().spec(spec).when().get("/{first}/{second}");
+        Map<String, Object> actualData= response.as(HashMap.class);
+
+        System.out.println(actualData);
+
+        //4. Step: Do Assortion
+        assertEquals(expectedDataMap.get("userId"),actualData.get("userId"));
+        assertEquals(expectedDataMap.get("title"),actualData.get("title"));
+        assertEquals(expectedDataMap.get("completed"),actualData.get("completed"));
+
+        assertEquals(expectedDataMap.get("Status code"),response.getStatusCode());
+        assertEquals(expectedDataMap.get("Via"),response.getHeader("Via"));
+        assertEquals(expectedDataMap.get("Server"),response.getHeader("Server"));
 
 
     }
