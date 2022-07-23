36a816af4ba5ba5c1ed73ddc4a86f858a7e5949c
Mehmet Eren Ergin <merenergin35@gmail.com>
2022-07-23 17:43:50 +0300

    23/07

--- src/test/java/get_requests/Get16.java
+++ src/test/java/get_requests/Get16.java
@@ -9,6 +9,7 @@ import java.util.Collections;
 import java.util.List;
 
 import static io.restassured.RestAssured.*;
+import static junit.framework.TestCase.*;
 import static org.hamcrest.Matchers.*;
 
 public class Get16 extends DummyRestApiBaseUrl {
@@ -24,6 +25,25 @@ public class Get16 extends DummyRestApiBaseUrl {
                     v) The name of the lowest age is "Tatyana Fitzpatrick"
                    vi) Total salary of all employees is 6,644,770
             */
+
+    /*
+    Given
+        https://dummy.restapiexample.com/api/v1/employees
+    When
+        User send Get Request to Url
+    Then
+        Status code is 200
+    And
+        There are 24 employees
+    And
+        "Tiger Nixon" and "Garrett Winters" are among the employees
+    And
+        The greatest age is 66
+    And
+        The name of the lowest age is "Tatyana Fitzpatrick"
+    And
+        Total salary of all employees is 6,644,770
+     */
 
     @Test
     public void get01(){
@@ -44,11 +64,35 @@ public class Get16 extends DummyRestApiBaseUrl {
 
         //iv) The greatest age is 66
         JsonPath json = response.jsonPath();
-        List<Integer> ageList = json.getList("data.findAll{it.id>0}.employee_age");
+        List<Integer> ageList = json.getList("data.findAll{it.id}.employee_age");
         System.out.println(ageList);
-
         Collections.sort(ageList);
         System.out.println(ageList);
+        System.out.println(ageList.get(ageList.size() - 1));
+        assertEquals(66, (int)ageList.get(ageList.size() - 1));
+
+        //v) The name of the lowest age is "Tatyana Fitzpatrick"
+        String groovyString = "data.findAll{it.employee_age == "+ageList.get(0)+"}.employee_name";
+        String minAgeEmployeeName = json.getString(groovyString);
+        System.out.println(minAgeEmployeeName);
+        assertEquals("[Tatyana Fitzpatrick]",minAgeEmployeeName);
+
+        //vi) Total salary of all employees is 6,644,770
+        List<Integer> salaryList = json.getList("data.findAll{it.id}.employee_salary");
+        System.out.println(salaryList);
+
+        //1.yol: foreach
+        int sumfor = 0;
+        for (int w:salaryList) {
+            sumfor+=w;
+        }
+        System.out.println(sumfor);
+        assertEquals(6644770,sumfor);
+
+        //2.yol: Lambda
+        int sumLambda = salaryList.stream().reduce(0,(t,u)->t+u);
+        //int sumLambda01 = salaryList.stream().reduce(0,Math::addExact);
+        assertEquals(6644770,sumLambda);
 
     }
 
